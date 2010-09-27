abacb508b9511a044c47af4739d2f49e2403dd98
dakrone <lee@writequit.org>
2010-09-27 12:20:46 -0600

    retab tabs into spaces and fix indention

--- src/cd_client/core.clj
+++ src/cd_client/core.clj
@@ -1,7 +1,7 @@
 (ns cd-client.core
   (:require [org.danlarkin.json :as json])
   (:require [clj-http.client :as http]
-	    [clojure.string :as string]))
+            [clojure.string :as string]))
 
 
 ; For testing purposes use localhost:8080
@@ -17,35 +17,35 @@
 (defn format-examples
   [examples]
   (map (fn
-	 [ex]
-	 (let [body (:body ex)]
-	   (assoc ex :body (-> body
-			       (.replaceAll "<pre>" "")
-			       (.replaceAll "</pre>" "")
-			       (.replaceAll "<p>" "")
-			       (.replaceAll "</p>" "")
-			       (.replaceAll "&gt;" ">")
-			       (.replaceAll "<br>" "")
-			       (.replaceAll "<br/>" "")
-			       (.replaceAll "<br />" "")
-			       (.replaceAll "\\\\r\\\\n" "\\\\n"))))) examples))
+       [ex]
+       (let [body (:body ex)]
+         (assoc ex :body (-> body
+                             (.replaceAll "<pre>" "")
+                             (.replaceAll "</pre>" "")
+                             (.replaceAll "<p>" "")
+                             (.replaceAll "</p>" "")
+                             (.replaceAll "&gt;" ">")
+                             (.replaceAll "<br>" "")
+                             (.replaceAll "<br/>" "")
+                             (.replaceAll "<br />" "")
+                             (.replaceAll "\\\\r\\\\n" "\\\\n"))))) examples))
 
 
 (defn examples
   "Return examples for a given namespace and method name."
   ([v]
      (let [m (meta v)
-	   ns (str (.name (:ns m)))
-	   name (str (:name m))]
+           ns (str (.name (:ns m)))
+           name (str (:name m))]
        (examples ns name)))
   ([ns name]
      (format-examples (json/decode-from-str (:body (http/get (str *examples-api* ns "/" name)))))))
 
 (defn pr-examples [v]
   (let [res (examples v)
-	m (meta v)
-	ns (str (.name (:ns m)))
-	name (str (:name m))]
+        m (meta v)
+        ns (str (.name (:ns m)))
+        name (str (:name m))]
     (println)
     (println "======================================== vvv")
     (doseq [ex res]
