f2d99a210a2b135e0ca0eeb9d6236d8fcdc1f0fa
Anthony Grimes <i@raynes.me>
2011-10-17 11:45:23 -0500

    Update data.json to 0.1.2

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A client for the clojuredocs API"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.2.1"]
-                 [org.clojure/data.json "0.1.1"]]
+                 [org.clojure/data.json "0.1.2"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
