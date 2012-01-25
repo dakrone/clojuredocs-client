7060fadb2d788b4008f9c74e7e3d97e9a34dd29a
Lee Hinman <lee@writequit.org>
2012-01-25 08:14:58 -0700

    Release 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.thnetos/cd-client "0.3.1"
+(defproject org.thnetos/cd-client "0.3.2"
   :description "A client for the clojuredocs API"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.2.1"]
