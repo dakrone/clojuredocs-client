b633cd563c0e86aab4a53264beff7ae98ee3568f
Lee Hinman <lee@writequit.org>
2012-03-30 10:23:44 -0600

    bump to 0.3.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.thnetos/cd-client "0.3.4"
+(defproject org.thnetos/cd-client "0.3.5-SNAPSHOT"
   :url "https://github.com/dakrone/clojuredocs-client"
   :description "A client for the clojuredocs API"
   :dependencies [[org.clojure/clojure "1.3.0"]
