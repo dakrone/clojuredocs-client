ec8001ca46bb3fbb3ebbb668378883fbbc75189f
Lee Hinman <lee@writequit.org>
2012-08-30 20:40:24 -0600

    Release 0.3.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.thnetos/cd-client "0.3.5-SNAPSHOT"
+(defproject org.thnetos/cd-client "0.3.5"
   :url "https://github.com/dakrone/clojuredocs-client"
   :description "A client for the clojuredocs API"
   :dependencies [[clj-http "0.5.3"]
